18f41a2ffbfc99e3b0ff2162179cc1cd930c6508
Einar Adalsteinsson <einar.adalsteinsson@rea-group.com>
2018-06-14 15:49:05 +1000

    Removes unwanted tests

--- src/client/components/SearchSpeech/__test__/SearchSpeech.spec.tsx
+++ src/client/components/SearchSpeech/__test__/SearchSpeech.spec.tsx
@@ -13,52 +13,6 @@ describe('<SearchSpeech />', () => {
 
         expect(tree).toMatchSnapshot();
     });
-
-    test('data', () => {
-        const data = {
-            assembly: 1,
-            issue: 2,
-            result: [{
-                id: 'speech-id',
-                assembly: {
-                    id: 1,
-                },
-                issue: {
-                    id: 3,
-                },
-                text: 'text',
-                period: {
-                    from: '2001-01-01',
-                    to: '2002-01-01',
-                },
-                iteration: '1',
-                type: 'type',
-                congressmanType: 'congressman type',
-                congressman: {
-                    id: 5,
-                    name: 'name',
-                    avatar: {
-                        templateSrc: '/path/to/image.jpg',
-                    },
-                    party: {
-                        id: 5,
-                        name: 'name',
-                        color: '123123',
-                    }
-                },
-            }],
-            isSearching: false,
-            onSearch: () => {},
-            onClear: () => {},
-        };
-        const tree = renderer.create(
-            <Router >
-                <SearchSpeech {...data} />
-            </Router>
-        ).toJSON();
-
-        expect(tree).toMatchSnapshot();
-    });
 });
 
 
